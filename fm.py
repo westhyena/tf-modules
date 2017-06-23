fix model calculation

fm.py
@@ -9,11 +9,17 @@ def get_batches(x, y, batch_size):
         end = min(start + batch_size, total_len)
         yield x[start:end], y[start:end]
         
-def matmul(a, b):
+def matmul_wrapper(a, b):
     if isinstance(a, tf.SparseTensor) or isinstance(b, tf.SparseTensor):
         return tf.sparse_tensor_dense_matmul(a, b)
     else:
         return tf.matmul(a,b)
+    
+def power_wrapper(a, p):
+    if isinstance(a, tf.SparseTensor):
+        return tf.SparseTensor(indices=a.indices, values=tf.pow(a.values, p), dense_shape=a.dense_shape)
+    else:
+        return tf.pow(a, p)
 
 class FM():
     
@@ -29,49 +35,71 @@ class FM():
         'hinge_loss ': tf.losses.hinge_loss
     }
     
-    def __init__(self, num_features, num_factors=8, sparse=False, optimizer='sgd', loss='mse', 
+    supported_input_type = ['dense', 'sparse', 'embedding']
+    
+    def __init__(self, num_features, num_factors=8, input_type='dense', optimizer='sgd', loss='mse', 
                  l2_regularizer=[0.0, 0.01, 0.01]):
+            
+        if input_type not in FM.supported_input_type:
+            raise Exception("Not supported input type {}. Currently support: {}"
+                            .format(input_type, ','.join(FM.supported_input_type)))
+            
+        if input_type == 'embedding' and False == isinstance(num_features, list):
+            raise Exception("num_features must be a list when input_type is set to embedding")
+        
         self.num_features = num_features
         self.num_factors = num_factors
-        self.sparse = sparse
+        self.input_type = input_type
         
         self._init_inputs()
         self._init_training_variables(num_features)
         self._init_model()
         self._init_loss(loss, l2_regularizer)
         self._init_optimizer(optimizer)
+        
+    def _has_input_type(self, input_type):
+        return self.input_type == input_type
     
     def _init_inputs(self):
         with tf.name_scope("inputs"):
-            if self.sparse:
+            if self._has_input_type('sparse'):
                 self.sparse_indices = tf.placeholder(tf.int64, shape=[None, 2], name='sparse_indices')
                 self.sparse_data = tf.placeholder(tf.float32, shape=[None], name='sparse_data')
                 self.sparse_shape = tf.placeholder(tf.int64, shape=[2], name='sparse_shape')
                 self.inputs = tf.SparseTensor(self.sparse_indices, self.sparse_data, self.sparse_shape)
+            elif self._has_input_type('embedding'):
+                self.inputs = tf.placeholder(tf.int32, shape=[None, None], name="inputs")
             else:
-                self.inputs = tf.placeholder(tf.float32, shape=[None, None], name="inputs")
+                self.inputs = tf.placeholder(tf.float32, shape=[None, self.num_features], name="inputs")
             self.targets = tf.placeholder(tf.float32, shape=[None], name="targets")
             
     def _init_training_variables(self, num_features):
         with tf.name_scope("training_variables"):
             self.bias = tf.Variable(tf.zeros(1), name="bias")
-            self.weights = tf.Variable(tf.zeros([1, num_features]), name="weights")
-            self.vectors = tf.Variable(tf.truncated_normal([self.num_factors, num_features], stddev=0.01), name="vectors")
+            if self._has_input_type('embedding'):
+                self.weights = tf.Variable(tf.zeros([1, num_features]), name="weights")
+                self.vectors = [tf.Variable(tf.truncated_normal([num, self.num_factors], stddev=0.01)
+                                           for num in self.num_features)]
+            else:
+                self.weights = tf.Variable(tf.zeros([1, num_features]), name="weights")
+                self.vectors = tf.Variable(tf.truncated_normal([self.num_factors, num_features], stddev=0.01),
+                                           name="vectors")
     
     def _init_model(self):
         with tf.name_scope("model"):
-            wx = matmul(self.inputs, tf.transpose(self.weights))
-            wxsum = tf.reduce_sum(wx, 1)
-            vecsum = 0
-            for f in range(self.num_factors):
-                #d = matmul(self.inputs, tf.reshape(self.vectors[f], shape=(self.vectors[f].shape[0].value, 1)))
-                d = tf.multiply(self.inputs, tf.transpose(self.vectors[f]))
-                fsum = tf.reduce_sum(d, 1)
-                fsum_sqr = tf.reduce_sum(tf.pow(d, 2), 1)
-
-                vecsum += tf.multiply(0.5, tf.add(tf.pow(fsum, 2), fsum_sqr))
-
-            self.logits = vecsum + wxsum + self.bias
+            
+            if self._has_input_type('embedding'):
+                for idx, vec in enumerate(self.vectors):
+                    tf.nn.embedding_lookup(vec, tf.slice(self.inputs, [0, idx], []))
+            else:
+                wx = matmul_wrapper(self.inputs, tf.transpose(self.weights))
+                wxsum = tf.reduce_sum(wx, 1)
+                
+                d = matmul_wrapper(self.inputs, tf.transpose(self.vectors))
+                dsqr = matmul_wrapper(power_wrapper(self.inputs, 2), tf.pow(tf.transpose(self.vectors), 2))
+                vecsum = tf.multiply(0.5, tf.reduce_sum(tf.subtract(tf.pow(d, 2), dsqr), axis=1))
+                
+                self.logits = vecsum + wxsum + self.bias
 
     def _init_loss(self, loss, l2_regularizer):
         loss_function = FM.supported_loss.get(loss, None)
@@ -102,7 +130,7 @@ class FM():
     def _batch_to_feeddict(self, batch_x, batch_y=None):
         feed_dict = {}
         
-        if self.sparse:
+        if self._has_input_type("sparse"):
             if False == scipy.sparse.issparse(batch_x):
                 raise Exception("Batch Data is not Sparse")
             coo = batch_x.tocoo()